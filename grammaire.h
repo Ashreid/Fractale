10c0e70838d3870d254fd1a6c3cbd017f3d9d692
Thibault Perrin <Shin@localhost.localdomain>
2013-10-22 11:08:32 +0200

    Finalisation de grammaire.h

--- grammaire.h
+++ grammaire.h
@@ -1,15 +1,17 @@
 #ifndef _GRAMMAIRE_H_
 #define _GRAMMAIRE_H
 
-#define MAX_CARAC 79
-#define MAX_REGLE 10
+#define MAX_CARAC 79                   /* Dimension max d'une ligne de fichier */
+#define MAX_REGLE 10                   /* Nombre max de regles */
 
 typedef struct {
-  char titre[MAX_CARAC] ;
-  int nb_dir ;
-  int dir ;
-  char axiome[MAX_CARAC] ;
-  char regle[MAX_CARAC][MAX_REGLE] ;
+  char titre[MAX_CARAC] ;              /* Nom de la grammaire */
+  int nb_dir ;                         /* Nombre de direction */
+  char axiome[MAX_CARAC] ;             /* Axiome de la grammaire */
+  char regle[MAX_CARAC][MAX_REGLE] ;   /* Regles de la grammaire */
+  int long_regle[MAX_CARAC] ;          /* Taille de la regle */
+  int turtle_dir_max ;                 /* Nombre de direction maximal */
+  int turtle_dir_init ;                /* Direction initiale de la tortue */
   
 } Grammaire ;
 
@@ -17,3 +19,4 @@ void grammaire_lire(char *nomfic, Grammaire *g);
 void grammaire_afficher(Grammaire g);
 int grammaire_trouver(Grammaire *g, char cmd);
  
+#endif 
