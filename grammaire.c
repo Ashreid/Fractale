2b5ac3b4f57a916dafa14e6494d7cc7db2e4c197
JULIEN HANS <julienhans@MacBook-Pro-de-JULIEN-HANS.local>
2013-10-22 11:19:49 +0200

    fonction lire grammaire faite

--- grammaire.c
+++ grammaire.c
@@ -2,17 +2,47 @@
 #include <stdlib.h>
 #include "grammaire.h"
 
-FILE *f_grammaire ;
 
 void grammaire_lire(char *nomfic, Grammaire *g) {
-  f_grammaire = fopen((*nomfic), "r") ;
+FILE *pfo ;
+
+  pfo = fopen((nomfic), "r") ;
   
-  while (fscanf(f_grammaire, "%s %d %d %s %s", titre, &nb_dir, &dir, axiome, regle) != (EOF)) { 
-    typedef Grammaire *grammaire ;
-  }
-  fclose(f_grammaire) ;
+if(!pfo) {
+	printf("Ouverture du fichier %s impossible\n", nomfic);
+	exit(2);
 }
-  
+
+/* lecture nom de a fractale */
+fscanf(pfo, "%s\n",g->nom);
+
+/* lecture nombre de directions */
+fscanf(pfo,"%d\n",&(g->turtle_dir_max));
+
+/* lecture de la direction initiale */
+fscanf(pfo,"%d\n",&(g->turtle_dir_init));
+
+/* lecture de l'axiome */
+fscanf(pfo,"%s\n",g->axiome);
+
+/* aucune regles au depart */
+g->nb_dir = 0;
+
+
+
+  while (fscanf(pfo, "%c ->%s\n",
+  				&(g->car[g->nb_dir]),
+  				g->regle[g->nb_dir]) != (EOF) { 
+    
+    /* calcul de la longueur de la partie droite de la regle */
+    g->long_regle[g->nb_dir] = strlen(g->regle[g->nb_dir]);
+    g->nb_dir++;
+  }
+
+  fclose(pfo);
+}
+ 
+
 void grammaire_afficher(Grammaire g) {
   f_grammaire = f_open((*nomfic), "r") ;
 
